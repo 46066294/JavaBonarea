3743ea571e4dc982c32b10f099bdc7cc47a2799f
Marc Cano <marc.cano@bonarea.com>
2021-05-04 12:43:26 +0200

    exemples if, else if vs switch

---
+++ src/main/java/com/area/multiplesopciones/Principal2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.area.multiplesopciones;
+
+/**
+ *
+ * @author marc.cano
+ */
+public class Principal2 {
+    
+    public static void main(String[] args) {
+        int number = 0;
+        
+        if(number > 0){
+            System.out.println("The number is positive");
+        } else if (number < 0){
+            System.out.println("The number is negative");
+        } else {
+            System.out.println("The number is 0");
+        }
+        
+    }// main
+}//Principal2
